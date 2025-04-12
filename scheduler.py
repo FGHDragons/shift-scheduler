42c718ded6596e87fd6a5e7dcbf37b0fbd2f882c
FGHDragons <jyasmine@protonmail.com>
2025-04-12 22:09:56 +0800

    Create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import random
+from config import EMPLOYEES, SHIFTS, SPECIAL_DAYS, VALID_PATTERNS
+
+class ShiftScheduler:
+    # ... (paste the full class code from earlier)
